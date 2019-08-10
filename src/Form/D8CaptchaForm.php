d2325114fd48019d886644a01d4ffd1334e06722
chmez <chmez070@gmail.com>
2019-08-10 19:51:51 +0300

    Issue #71: Use injected services in the installation form for captcha keys.

--- src/Form/D8CaptchaForm.php
+++ src/Form/D8CaptchaForm.php
@@ -2,13 +2,43 @@
 
 namespace Drupal\d8\Form;
 
+use Drupal\Core\Config\ConfigFactoryInterface;
 use Drupal\Core\Form\FormStateInterface;
+use Drupal\Core\StringTranslation\TranslationInterface;
 use Drupal\recaptcha\Form\ReCaptchaAdminSettingsForm;
+use Symfony\Component\DependencyInjection\ContainerInterface;
 
 /**
  * Configure reCAPTCHA settings for this profile.
  */
 class D8CaptchaForm extends ReCaptchaAdminSettingsForm {
+
+  /**
+   * Constructs a D8CaptchaForm object.
+   *
+   * @param \Drupal\Core\Config\ConfigFactoryInterface $config_factory
+   *   The factory for configuration objects.
+   * @param \Drupal\Core\StringTranslation\TranslationInterface $string_translation
+   *   The string translation service.
+   */
+  public function __construct(
+    ConfigFactoryInterface $config_factory,
+    TranslationInterface $string_translation
+  ) {
+    parent::__construct($config_factory);
+
+    $this->stringTranslation = $string_translation;
+  }
+
+  /**
+   * {@inheritdoc}
+   */
+  public static function create(ContainerInterface $container) {
+    return new static(
+      $container->get('config.factory'),
+      $container->get('string_translation')
+    );
+  }
 
   /**
    * {@inheritdoc}
