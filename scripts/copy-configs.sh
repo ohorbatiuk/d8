5b496a2ad7a2a6e2fbf90cade322ee4c7d97e0cb
chmez <chmez070@gmail.com>
2022-06-24 08:52:40 +0300

    Issue #183: Change version again

--- scripts/copy-configs.sh
+++ scripts/copy-configs.sh
@@ -4,8 +4,8 @@ for path in $(find . -name install); do
   echo "Check files in folder $path"
   for filename in $(ls $path); do
     if [ -f configs/$filename ]; then
-      cp configs/$filename $path
       echo "- Copy file $filename"
+      cp configs/$filename $path
     fi
   done
 done
