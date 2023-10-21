4ff54efc9710bd14f15f5146e7be8d06fcffc74b
Oleksandr Horbatiuk <chmez070@gmail.com>
2023-10-21 03:38:17 +0300

    Issue #260: Convert plain text in an E-mail letter to HTML

---
+++ modules/features/d8_log/src/Plugin/EmailAdjuster/LogEmailAdjuster.php
@@ -0,0 +1,92 @@
+<?php
+
+namespace Drupal\d8_log\Plugin\EmailAdjuster;
+
+use Drupal\Component\Render\MarkupInterface;
+use Drupal\Core\Plugin\ContainerFactoryPluginInterface;
+use Drupal\Core\Render\RendererInterface;
+use Drupal\symfony_mailer\EmailInterface;
+use Drupal\symfony_mailer\Processor\EmailAdjusterBase;
+use Symfony\Component\DependencyInjection\ContainerInterface;
+
+/**
+ * Defines the Log Email Adjuster.
+ *
+ * @EmailAdjuster(
+ *   id = "log",
+ *   label = @Translation("Log"),
+ *   description = @Translation("Error & Exception to HTML."),
+ *   weight = 800,
+ * )
+ */
+class LogEmailAdjuster extends EmailAdjusterBase implements ContainerFactoryPluginInterface {
+
+  /**
+   * The renderer.
+   */
+  protected readonly RendererInterface $renderer;
+
+  /**
+   * {@inheritdoc}
+   */
+  public static function create(
+    ContainerInterface $container,
+    array $configuration,
+    $plugin_id,
+    $plugin_definition
+  ): static {
+    $instance = new static($configuration, $plugin_id, $plugin_definition);
+
+    $instance->renderer = $container->get('renderer');
+
+    return $instance;
+  }
+
+  private function cell(string $content): array {
+    return [
+      '#markup' => preg_replace('#(\w+[/\\\]+)(\w+)#', '$1<wbr>$2', $content),
+      '#allowed_tags' => ['wbr'],
+    ];
+  }
+
+  /**
+   * {@inheritdoc}
+   */
+  public function postRender(EmailInterface $email): void {
+    $body = preg_split('/\s*\n+\s*/', trim($email->getHtmlBody()));
+
+    $elements[] = [
+      '#theme' => 'table',
+      '#rows' => array_map(
+        function (string $item): array {
+          $item = explode(':', $item, 2);
+          $item[1] = ['data' => $this->cell($item[1])];
+          return $item;
+        },
+        array_slice($body, 2, 10),
+      ),
+    ];
+
+    $elements[] = [
+      '#theme' => 'item_list',
+      '#list_type' => 'ol',
+      '#items' => array_map(
+        fn(string $item): array => $this
+          ->cell(preg_replace('/^#\d+\s+/', '', $item)),
+        array_slice($body, 12, -1),
+      ),
+    ];
+
+    $email->setHtmlBody(
+      implode(
+        '<hr>',
+        array_map(
+          fn(array $element): MarkupInterface => $this->renderer
+            ->render($element),
+          $elements,
+        ),
+      ),
+    );
+  }
+
+}
