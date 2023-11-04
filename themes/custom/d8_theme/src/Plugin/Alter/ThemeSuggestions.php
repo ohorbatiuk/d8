7b0e603d2cb60700cf45960101f8351a493f947f
Oleksandr Horbatiuk <chmez070@gmail.com>
2023-11-04 03:34:41 +0200

    Issue #280: Use trait in theme

--- themes/custom/d8_theme/src/Plugin/Alter/ThemeSuggestions.php
+++ themes/custom/d8_theme/src/Plugin/Alter/ThemeSuggestions.php
@@ -4,7 +4,7 @@ namespace Drupal\d8_theme\Plugin\Alter;
 
 use Drupal\bootstrap\Plugin\Alter\ThemeSuggestions as ThemeSuggestionsBase;
 use Drupal\Core\Plugin\ContainerFactoryPluginInterface;
-use Drupal\Core\Routing\RouteMatchInterface;
+use Drupal\service\RouteMatchTrait;
 use Symfony\Component\DependencyInjection\ContainerInterface;
 
 /**
@@ -16,10 +16,7 @@ use Symfony\Component\DependencyInjection\ContainerInterface;
  */
 class ThemeSuggestions extends ThemeSuggestionsBase implements ContainerFactoryPluginInterface {
 
-  /**
-   * The route match.
-   */
-  private readonly RouteMatchInterface $routeMatch;
+  use RouteMatchTrait;
 
   /**
    * {@inheritdoc}
@@ -29,19 +26,16 @@ class ThemeSuggestions extends ThemeSuggestionsBase implements ContainerFactoryP
     array $configuration,
     $plugin_id,
     $plugin_definition
-  ): self {
-    $instance = new static($configuration, $plugin_id, $plugin_definition);
-
-    $instance->routeMatch = $container->get('current_route_match');
-
-    return $instance;
+  ): static {
+    return (new static($configuration, $plugin_id, $plugin_definition))
+      ->addRouteMatch($container);
   }
 
   /**
    * Dynamic alter method for "maintenance_page".
    */
   protected function alterMaintenance_page(): void {
-    if ($this->routeMatch->getRouteName() !== 'system.db_update') {
+    if ($this->routeMatch()->getRouteName() !== 'system.db_update') {
       $this->addSuggestion($this->originalHook . '__guest');
     }
   }
