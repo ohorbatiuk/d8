5efd1a7909a0f0dea9c315a869ce9599e3cd2e95
Oleksandr Horbatiuk <oleksandr.v.horbatiuk@gmail.com>
2024-09-05 01:41:13 +0300

    Issue #313: Create a link formatter that removes the URL scheme and default sub-domain from the link title

---
+++ modules/features/d8_link/src/Plugin/Field/FieldFormatter/D8LinkFormatter.php
@@ -0,0 +1,39 @@
+<?php
+
+namespace Drupal\d8_link\Plugin\Field\FieldFormatter;
+
+use Drupal\Core\Url;
+use Drupal\d8_link\D8Url;
+use Drupal\link\LinkItemInterface;
+use Drupal\link\Plugin\Field\FieldFormatter\LinkFormatter;
+
+/**
+ * Plugin implementation of the 'd8_link' formatter.
+ *
+ * @FieldFormatter(
+ *   id = "d8_link",
+ *   label = @Translation("D8+ Link"),
+ *   description = @Translation("Delete the URL scheme and default sub-domain from the link title."),
+ *   field_types = {
+ *     "link"
+ *   }
+ * )
+ */
+class D8LinkFormatter extends LinkFormatter {
+
+  /**
+   * {@inheritdoc}
+   */
+  protected function buildUrl(LinkItemInterface $item): Url {
+    if (($url =  parent::buildUrl($item))->isExternal()) {
+      $url = D8Url::fromUri($url->getUri(), $url->getOptions());
+
+      $modify = &drupal_static('d8_link');
+
+      $modify = TRUE;
+    }
+
+    return $url;
+  }
+
+}
