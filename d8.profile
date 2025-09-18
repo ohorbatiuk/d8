dba55413c2f88f05b02010101fe8a95bc113ce83
Oleksandr Horbatiuk <oleksandr.v.horbatiuk@gmail.com>
2025-09-19 00:07:05 +0300

    Issue #377: Update the form alter hook and hide messages about problems sending emails

--- d8.profile
+++ d8.profile
@@ -1,7 +1,5 @@
 <?php
 
-declare(strict_types=1);
-
 /**
  * @file
  * Provides installation profile for Drupal 11.
@@ -9,17 +7,3 @@ declare(strict_types=1);
  * It is based on the most common modules and themes that form the basis for
  * creating a stable site.
  */
-
-use Drupal\Core\Form\FormStateInterface;
-use Drupal\RecipeKit\Installer\Hooks;
-
-/**
- * Implements hook_form_alter().
- */
-function d8_form_alter(
-  array &$form,
-  FormStateInterface $form_state,
-  string $form_id,
-): void {
-  Hooks::formAlter($form, $form_state, $form_id);
-}
--- src/Hook/D8Hooks.php
+++ src/Hook/D8Hooks.php
@@ -1,56 +1,28 @@
 <?php
 
+declare(strict_types=1);
+
 namespace Drupal\d8\Hook;
 
-use Drupal\Core\Config\ConfigFactoryInterface;
 use Drupal\Core\Form\FormStateInterface;
 use Drupal\Core\Hook\Attribute\Hook;
-use Drupal\Core\Url;
-use Symfony\Component\HttpFoundation\RequestStack;
+use Drupal\RecipeKit\Installer\Hooks;
 
 /**
  * Hook implementations for d8.
  */
-final readonly class D8Hooks {
+final class D8Hooks {
 
   /**
-   * D8Hooks constructor.
-   *
-   * @param \Symfony\Component\HttpFoundation\RequestStack $requestStack
-   *   The request stack.
-   * @param \Drupal\Core\Config\ConfigFactoryInterface $configFactory
-   *   The configuration factory.
+   * Implements hook_form_alter().
    */
-  public function __construct(
-    private RequestStack $requestStack,
-    private ConfigFactoryInterface $configFactory,
-  ) {}
-
-  /**
-   * Implements hook_form_FORM_ID_alter().
-   */
-  #[Hook('form_user_login_form_alter')]
-  public function formUserLoginFormAlter(
+  #[Hook('form_alter')]
+  public function formAlter(
     array &$form,
     FormStateInterface $form_state,
+    string $form_id,
   ): void {
-    $form['#submit'][] = [$this, 'submit'];
-  }
-
-  /**
-   * Redirect on the front page after logging in if this page is set.
-   */
-  public function submit(
-    array &$form,
-    FormStateInterface $form_state,
-  ): void {
-    if (!$this->requestStack->getCurrentRequest()->request->has('destination')) {
-      $path = $this->configFactory->get('system.site')->get('page.front');
-
-      if (!empty($path)) {
-        $form_state->setRedirectUrl(Url::fromUserInput($path));
-      }
-    }
+    Hooks::formAlter($form, $form_state, $form_id);
   }
 
 }
